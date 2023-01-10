d5e98ccbc7a6ba7b9521b38ba85685d304ddd2bb
Galen Sho Sato <galen.sato@gmail.com>
2023-01-10 14:29:16 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+function saturdayFun(funtime) {
+    let x 
+    if (funtime) {
+        return x = `This Saturday, I want to ${funtime}!`
+    }
+    else {
+        funtime = 'roller-skate'
+        return x = `This Saturday, I want to ${funtime}!`
+    }
+}
+
+const mondayWork = function (sadtime) {
+    let x
+    if (sadtime) {
+        return x = `This Monday, I will ${sadtime}.`
+    }
+    else {
+        sadtime = 'go to the office'
+        return x = `This Monday, I will ${sadtime}.`
+    }
+}
+
+function wrapAdjective (symbol = '*') {
+    return function (encourage = 'special') {
+        return `You are ${symbol}${encourage}${symbol}!`
+    }
+}
